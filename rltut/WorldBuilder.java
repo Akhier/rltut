116befa87531a02e07a2a02d3e7a15e58327412a
Akhier <akherthedragonhearted@gmail.com>
2017-02-11 12:10:47 -0500

    roguelike tutorial 03e: scrolling through random caves
    Huh, I apperently didn't save before the last commit...
    
    Yeah the new stuff is randomizeTiles

--- rltut/WorldBuilder.java
+++ rltut/WorldBuilder.java
@@ -1,5 +1,26 @@
 package rltut;
 
 public class WorldBuilder {
+	private int width;
+	private int height;
+	private Tile[][] tiles;
 
+	public WorldBuilder(int width, int height) {
+		this.width = width;
+		this.height = height;
+		this.tiles = new Tile[width][height];
+	}
+
+	public World build() {
+		return new World(tiles);
+	}
+
+	private WorldBuilder randomizeTiles() {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				tiles[x][y] = Math.random() < 0.5 ? Tile.FLOOR : Tile.WALL;
+			}
+		}
+		return this;
+	}
 }
