1b15f4f8de35a8038e23e9908a06b56ddfde6afb
Akhier <akherthedragonhearted@gmail.com>
2017-02-27 16:11:47 -0500

    roguelike tutorial 07a: z levels and deeper caves
    Adding a basic point class with an x, y, and z

---
+++ rltut/Point.java
@@ -0,0 +1,17 @@
+package rltut;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Point {
+	public int x;
+	public int y;
+	public int z;
+
+	public Point(int x, int y, int z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
