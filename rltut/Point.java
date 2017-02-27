e1d1c228c42b08afe7d58c0aa2abb736d08ba9e9
Akhier <akherthedragonhearted@gmail.com>
2017-02-27 17:11:03 -0500

    roguelike tutorial 07c: z levels and deeper caves
    Making the list of what would be the 8 adjacent tiles. The shuffle is to
    remove the bias that would happen if you always started in the upper
    left.

--- rltut/Point.java
+++ rltut/Point.java
@@ -46,4 +46,20 @@ public class Point {
 		}
 		return true;
 	}
+
+	public List<Point> neighbors8() {
+		List<Point> points = new ArrayList<Point>();
+
+		for (int ox = -1; ox < 2; ox++) {
+			for (int oy = -1; oy < 2; oy++) {
+				if (ox == 0 && oy == 0) {
+					continue;
+				}
+				points.add(new Point(x + ox, y + oy, x));
+			}
+		}
+
+		Collections.shuffle(points);
+		return points;
+	}
 }
